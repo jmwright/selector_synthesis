132d23473553cb79c2f231317df911491d4a4b55
Jeremy Wright <wrightjmf@gmail.com>
2022-01-26 10:36:41 -0500

    Cleaned up existing test to be grouped more logically

--- tests/test_vector_synth.py
+++ tests/test_vector_synth.py
@@ -1,23 +1,41 @@
 import pytest
 from vector_based_synth import *
 
+
+def test_to_vector():
+    """
+    Tests the capability of converting tuples and lists
+    to CadQuery Vectors.
+    """
+
+     # Test a tuple
+    v1 = (1, 1, 0)
+    v2 = to_vector(v1)
+    assert(v2 == Vector(1, 1, 0))
+
+    # Test a list
+    v1 = [1, 1, 0]
+    v2 = to_vector(v1)
+    assert(v2 == Vector(1, 1, 0))
+
+    # Test Vector
+    v1 = Vector(1, 1, 0)
+    v2 = to_vector(v1)
+    assert(v2 == v1)
+
+
 def test_are_vectors_parallel():
-    # Test tuples that are parallel
+    """
+    Tests the method that determines if two CadQuery Vectors
+    are parallel.
+    """
+
+    # Test vectors that are parallel
     v1 = (1, 1, 0)
     v2 = (2, 2, 0)
     assert(are_vectors_parallel(v1, v2) == True)
 
-    # Test lists that are parallel
-    v1 = [1, 1, 0]
-    v2 = [2, 2, 0]
-    assert(are_vectors_parallel(v1, v2) == True)
-
-    # Test Vectors that are parallel
-    v1 = Vector(1, 1, 0)
-    v2 = Vector(2, 2, 0)
-    assert(are_vectors_parallel(v1, v2) == True)
-
-    # Test Vectors that are not parallel
-    v1 = Vector(1, 1, 0)
-    v2 = Vector(2, 5, 0)
+    # Test vectors that are not parallel
+    v1 = (1, 1, 0)
+    v2 = (2, 5, 0)
     assert(are_vectors_parallel(v1, v2) == False)
